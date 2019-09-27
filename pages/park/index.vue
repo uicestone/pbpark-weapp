7b086fedd537dd4677212296691927353ea61074
koishi <ncysatnaf@gmail.com>
2019-09-27 15:57:10 +0800

    update ui

--- pages/park/index.vue
+++ pages/park/index.vue
@@ -5,7 +5,7 @@
       view 远香湖党建主题公园
     view.relative
       img.map(:src="mapUrl" mode="scaleToFill" style="height: 750upx;z-index: -1;")
-      img.absolute.point-img(:src="item.src" @click="goDetail"  mode="top" v-for="(item,index) in points" :index="index" :style="[{'left': item.x, 'top': item.y}]")
+      img.absolute.point-img(:src="item.src" :class="'point-img-'+index" @click="goDetail"  mode="widthFix" v-for="(item,index) in points" :index="index" :style="[{'left': item.x, 'top': item.y}]")
     view.fixed.flex.justify-center.response(style="bottom:-10upx;left:0" @click="goExam")
       img(:src="btnUrl" mode="widthFix" style="width: 300upx;")
 </template>
@@ -19,19 +19,34 @@ export default {
       btnUrl: "/static/change-map-btn.png",
       points: [
         {
-          x: uni.upx2px(53 * 6) + "px",
-          y: uni.upx2px(280 * 6) + "px",
+          x: uni.upx2px(53 * 2) + "px",
+          y: uni.upx2px(280 * 2) + "px",
           src: "/static/p1.png"
         },
         {
-          x: uni.upx2px(136 * 6) + "px",
-          y: uni.upx2px(220 * 6) + "px",
+          x: uni.upx2px(146 * 2) + "px",
+          y: uni.upx2px(220 * 2) + "px",
           src: "/static/p2.png"
         },
         {
-          x: uni.upx2px(260 * 6) + "px",
-          y: uni.upx2px(220 * 6) + "px",
+          x: uni.upx2px(260 * 2) + "px",
+          y: uni.upx2px(185 * 2) + "px",
           src: "/static/p3.png"
+        },
+        {
+          x: uni.upx2px(280 * 2) + "px",
+          y: uni.upx2px(140 * 2) + "px",
+          src: "/static/p4.png"
+        },
+        {
+          x: uni.upx2px(183 * 2) + "px",
+          y: uni.upx2px(124 * 2) + "px",
+          src: "/static/p5.png"
+        },
+        {
+          x: uni.upx2px(103 * 2) + "px",
+          y: uni.upx2px(170 * 2) + "px",
+          src: "/static/p6.png"
         }
       ]
     };
@@ -56,7 +71,20 @@ export default {
   .map
     width 750upx
   .point-img
-    zoom 0.3333
+    width 247 * 0.66666upx
+    height 147 * 0.66666upx
+  .point-img-0
+    width 333 * 0.66666upx
+    height 70 * 0.66666upx
+  .point-img-1
+    width 247 * 0.66666upx
+    height 147 * 0.66666upx
+  .point-img-2
+    width 247 * 0.66666upx
+    height 147 * 0.66666upx
+  .point-img-5
+    width 332 * 0.66666upx
+    height 69 * 0.66666upx
   .desc
     color #595757
     font-weight bold
