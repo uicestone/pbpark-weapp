e15aaf3ce75a87c7a3b7d9dfa371c48c381b853e
koishi <ncysatnaf@gmail.com>
2019-09-27 14:36:20 +0800

    update map

--- pages/park/index.vue
+++ pages/park/index.vue
@@ -3,9 +3,9 @@
     bg
     view(style="margin-top: 100upx")
       view 远香湖党建主题公园
-    view
-      img.map(:src="mapUrl" mode="scaleToFill" style="height: 750upx;z-index: -1;" @click="goDetail")
-      img.absolute.point-img(:src="item.src" mode="widthFix" v-for="(item,index) in points" :index="index" :style="[{'left': item.x, 'top': item.y}]")
+    view.relative
+      img.map(:src="mapUrl" mode="scaleToFill" style="height: 750upx;z-index: -1;")
+      img.absolute.point-img(:src="item.src" @click="goDetail"  mode="top" v-for="(item,index) in points" :index="index" :style="[{'left': item.x, 'top': item.y}]")
     view.fixed.flex.justify-center.response(style="bottom:-10upx;left:0" @click="goExam")
       img(:src="btnUrl" mode="widthFix" style="width: 300upx;")
 </template>
@@ -19,9 +19,19 @@ export default {
       btnUrl: "/static/change-map-btn.png",
       points: [
         {
-          x: 67 + "px",
-          y: 292 + "px",
+          x: uni.upx2px(53 * 6) + "px",
+          y: uni.upx2px(280 * 6) + "px",
           src: "/static/p1.png"
+        },
+        {
+          x: uni.upx2px(136 * 6) + "px",
+          y: uni.upx2px(220 * 6) + "px",
+          src: "/static/p2.png"
+        },
+        {
+          x: uni.upx2px(260 * 6) + "px",
+          y: uni.upx2px(220 * 6) + "px",
+          src: "/static/p3.png"
         }
       ]
     };
@@ -45,6 +55,8 @@ export default {
 .park
   .map
     width 750upx
+  .point-img
+    zoom 0.3333
   .desc
     color #595757
     font-weight bold
